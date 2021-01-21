28bbe62186d1183d667d14e95d57c095652dd4c8
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2021-01-21 09:01:52 +0700

    update

--- lib/view/page/home.dart
+++ lib/view/page/home.dart
@@ -95,8 +95,8 @@ class HomePage extends Page<HomeBloc> {
             subtitle: 'Wujudkan Ide Bisnis Anda',
             list: bloc.listOfContainer1,
           ),
-          ContainerRow(),
-          ContainerRow(),
+          const ContainerRow(),
+          const ContainerRow(),
           const SizedBox(height: 24),
           MainButton(
             text: 'LIHAT SEMUA',
@@ -115,49 +115,62 @@ class HomePage extends Page<HomeBloc> {
             children: [
               Text('TERPOPULER', style: theme.textTheme.subtitle1),
               const SizedBox(height: 12),
-              Container(
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  color: const Color(0xFFEAF2F9),
-                  borderRadius: BorderRadius.circular(12)
-                ),
-                child: SizedBox(
-                  height: 150,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Image.asset('assets/background/content.jpg',
-                        width: 150,
-                        height: 150,
-                      ),
-                      const Spacer(),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Chip(
-                            backgroundColor: theme.primaryColor,
-                            label: Text('Rp. 150.000',style: theme.textTheme.subtitle2.copyWith(
-                              color: Colors.white
-                            )), 
-                          ),
-                          SizedBox(
-                            width: 160,
-                            child: Text('Pelatihan Untuk Membuat Furniture', style: theme.textTheme.bodyText1),
-                          ),
-                          Text('Member', style: theme.textTheme.subtitle2),
-                        ],
-                      ),
-                      const Spacer(),
-                    ],
-                  ),
-                ),
-              )
+              ...List.generate(3, (index) => ContainerTile()),
             ],
+          ),
+          MainButton(
+            text: 'LIHAT SEMUA',
+            color: theme.accentColor,
           ),
         ],
       ),
+    );
+  }
+}
+
+class ContainerTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: const Color(0xFFEAF2F9),
+        borderRadius: BorderRadius.circular(12)
+      ),
+      child: SizedBox(
+        height: 150,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset('assets/background/content.jpg',
+              width: 150,
+              height: 150,
+            ),
+            const Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Chip(
+                  backgroundColor: theme.primaryColor,
+                  label: Text('Rp. 150.000',style: theme.textTheme.subtitle2.copyWith(
+                    color: Colors.white
+                  )), 
+                ),
+                SizedBox(
+                  width: 160,
+                  child: Text('Pelatihan Untuk Membuat Furniture', style: theme.textTheme.bodyText1),
+                ),
+                Text('Member', style: theme.textTheme.subtitle2),
+              ],
+            ),
+            const Spacer(),
+          ],
+        ),
+      )
     );
   }
 }
